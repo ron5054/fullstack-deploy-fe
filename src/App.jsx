2b58c31da6fbd913abb0810e58198045f55732a9
Yaron Levy <ron5054@gmail.com>
2024-06-15 13:46:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('')
+  console.log(process.env.NODE_ENV)
+
+  const BASE_URL = 'http://localhost:3030/'
+
+  const getMessage = async () => {
+    try {
+      const response = await fetch(BASE_URL)
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+
+      const data = await response.text()
+      setMessage(data)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  return (
+    <>
+      <h1>{message}</h1>
+      <div className='card'>
+        <button onClick={getMessage}>Get message from backend</button>
+      </div>
+    </>
+  )
+}
+
+export default App
